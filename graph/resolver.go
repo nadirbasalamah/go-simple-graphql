4f3b2d6a13033fa7a7612de7f4b0d02239c94018
nadirbasalamah <nadir008basalamah@gmail.com>
2022-06-22 16:36:04 +0700

    feat(resolver): Adding resolver for products.

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "github.com/nadirbasalamah/go-simple-graphql/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	products []*model.Product
+}
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,17 +6,28 @@ package graph
 import (
 	"context"
 	"fmt"
+	"math/rand"
 
 	"github.com/nadirbasalamah/go-simple-graphql/graph/generated"
 	"github.com/nadirbasalamah/go-simple-graphql/graph/model"
 )
 
 func (r *mutationResolver) CreateProduct(ctx context.Context, input model.NewProduct) (*model.Product, error) {
-	panic(fmt.Errorf("not implemented"))
+	var newProduct *model.Product = &model.Product{
+		ID:          fmt.Sprintf("T%d", rand.Int()),
+		Name:        input.Name,
+		Description: input.Description,
+		Price:       input.Price,
+		Quantity:    input.Quantity,
+	}
+
+	r.products = append(r.products, newProduct)
+
+	return newProduct, nil
 }
 
 func (r *queryResolver) Products(ctx context.Context) ([]*model.Product, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.products, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
@@ -27,16 +38,3 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
-}
